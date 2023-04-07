c75f5b09fb658a78127340abf23531a917354778
himabindubontha333 <bonthahimabindu123@gmail.com>
2023-04-07 15:43:04 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app.models import *
 # Create your views here.
+from django.db.models.functions import Length
 def display_topic(request):
     LOT=Topic.objects.all()
     LOT=Topic.objects.filter(topic_name='Cricket')
@@ -8,6 +9,8 @@ def display_topic(request):
     return render(request,'display_topic.html',context=d)
 def display_webpage(request):
     LOW=Webpage.objects.all()
+    LOW=Webpage.objects.all().order_by(Length('name'))
+    LOW=Webpage.objects.all().order_by(Length('name').desc())
     d={'webpage':LOW}
     return render(request,'display_webpage.html',context=d)
 def Access_records(request):
